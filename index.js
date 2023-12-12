3820df317fa784dd27008c034d86d1eab05fbe0c
Lamech74 <omwegalamech@gmail.com>
2023-12-12 11:08:16 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,39 @@
 
 // Write your code here
+// Assuming you have values for num1 and num2
+let num1 = 31;
+let num2 = 2;
+
+// Creating the variable 'multiply' with the desired equation
+let multiply = num1 * num2;
+
+// Checking if the result is 62
+console.log(multiply); // Output: 62
+
+
+// Generating a random integer greater than 0
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+// Checking the value of the 'random' variable
+console.log(random);
+
+
+// Assuming you have values for num3 and num4
+let num3 = 18;
+let num4 = 7;
+
+// Creating the variable 'mod' with the desired equation
+let mod = (num3 % num4 + num4) % num4;
+
+// Checking if the remainder is 4
+console.log(mod); // Output: 4
+
+
+// Assuming you have a set of numbers
+let numbers = [5, 12, 8, 20, 15];
+
+// Creating the variable 'max' with the highest number
+let max = Math.max(...numbers);
+
+// Checking the value of the 'max' variable
+console.log(max); // Output: 20
